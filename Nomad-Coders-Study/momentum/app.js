3bdcc09e0e27690f5d4f1a709c3ad844ee368642
류종현 <31603341+rjh211@users.noreply.github.com>
2022-01-06 20:11:52 +0900

    Add files via upload
    second day

--- Nomad-Coders-Study/momentum/app.js
+++ Nomad-Coders-Study/momentum/app.js
@@ -1,1 +1,27 @@
-alert("hi")
+const test = document.querySelector("div.hello:first-child h1")
+console.dir(test)
+
+test.innerText = "Hello"
+test.style.color = "red"
+
+function MouseClick(){
+    console.log("title was clicked!")
+}
+
+test.onclick = ()=>console.log("title was clicked!");
+test.addEventListener("mouseenter", ()=>test.innerText = "Mouse is Enter!")
+test.addEventListener("mouseleave", ()=>test.innerText = "Mouse is gone!")
+console.dir(test);
+
+window.addEventListener("resize", ()=>{
+    document.body.style.backgroundColor = "tomato";
+});
+window.addEventListener("copy", ()=>{
+    alert("copier!");
+});
+window.addEventListener("offline", ()=>{
+    alert("SOS no WIFI")
+});
+window.addEventListener("online", ()=>{
+    alert("ALL GOOD");
+});
