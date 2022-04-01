9b05c73f95f6b3639293e268dea5767bdaca727d
rjh211 <rjh211@en-core.com>
2022-04-01 09:29:31 +0900

    Change java version 8 to 11

---
+++ java11.bat
@@ -0,0 +1,4 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Java\jdk-11
+set PATH=%JAVA_HOME%\bin;%PATH%
+echo Display Java11 version
---
+++ java8.bat
@@ -0,0 +1,4 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_281
+set PATH=%JAVA_HOME%\bin;%PATH%
+echo Display Java8 version
