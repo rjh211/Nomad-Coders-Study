45cc423d11c1a80e81f3d23721a1502ecd7a2860
류종현 <rjh211@naver.com>
2022-06-27 21:25:24 +0900

    Separate MyView

---
+++ Spring/spring-study/src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -0,0 +1,29 @@
+package hello.servlet.web.frontcontroller.v2.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberSaveControllerV2 implements ControllerV2 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    public MyView process(HttpServletResponse response, HttpServletRequest request) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        //Model에 데이터 보관
+        request.setAttribute("member", member); //request내부의 저장소에 member객체를 저장
+
+        return new MyView("/WEB-INF/views/save-result.jsp");
+    }
+}
