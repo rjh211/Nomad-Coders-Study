3ae8018842924d3d2ef0a04d9355d43f49025b98
류종현 <rjh211@naver.com>
2022-07-30 22:58:37 +0900

    Add item add form

--- Spring/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ Spring/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -44,5 +45,15 @@ public class BasicItemController {
     public void init(){
         repository.save(new Item("ItemA", 10000, 10));
         repository.save(new Item("ItemB", 20000, 20));
+    }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add") //같은 URL호출이여도 호출 방법에 따라 기능이 달라지게 설계
+    public String save(){
+        return "basic/addForm";
     }
 }
