d7ee2db298ffc2e0272ac5996affacf236ece83b
류종현 <rjh211@naver.com>
2022-08-08 20:14:45 +0900

    Add Singleton Pattern

--- Nestjs/letsStart/src/app.ts
+++ Nestjs/letsStart/src/app.ts
@@ -1,27 +1,50 @@
 import * as express from "express";
 import catsRouter from "./cats/cats.route";
 
-const app: express.Express = express();
-const port: number = 8000;
+class Server {
+  public app: express.Application;
+  private port: number = 8000;
 
-// logging middleware
-app.use((req, res, next) => {
-  //미들웨어 함수 제작, 요청 -> 미들웨어 -> 라우터 -> 서버 순으로 전달이됨(모든 경로에 대해 사용가능)
-  console.log("middle ware");
-  next();
-});
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
 
-//Express에서 Json을 읽을수 있도록(req.body) middleware 추가
-app.use(express.json());
+  private setRoute() {
+    //라우터 세팅
+    this.app.use(catsRouter); //cats.route.ts의  router를 미들웨어 형태로 사용
+  }
 
-app.use(catsRouter); //cats.route.ts의  router를 미들웨어 형태로 사용
+  private setMiddleware() {
+    //미들웨어 세팅
+    // logging middleware
+    this.app.use((req, res, next) => {
+      //미들웨어 함수 제작, 요청 -> 미들웨어 -> 라우터 -> 서버 순으로 전달이됨(모든 경로에 대해 사용가능)
+      console.log("middle ware");
+      next();
+    });
 
-//404 middleware
-app.use((req, res, next) => {
-  //아무것도 찾지 못한 후, 마지막 미들웨어를 만나게 하여 에러 처리
-  res.send({ error: "404 not found error" });
-});
+    //Express에서 Json을 읽을수 있도록(req.body) middleware 추가
+    this.app.use(express.json());
 
-app.listen(port, () => {
-  console.log("server is on ...");
-});
+    this.setRoute();
+
+    //404 middleware
+    this.app.use((req, res, next) => {
+      //아무것도 찾지 못한 후, 마지막 미들웨어를 만나게 하여 에러 처리
+      res.send({ error: "404 not found error" });
+    });
+  }
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log("server is on ...");
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();
--- Nestjs/letsStart/src/cats/cats.route.ts
+++ Nestjs/letsStart/src/cats/cats.route.ts
@@ -1,141 +1,33 @@
 //route 관리를 위한 ts
 import { Cat, CatType } from "../cats/cats.model";
 import { Router } from "express";
+import {
+  readAllcat,
+  readCat,
+  creatCat,
+  updateCat,
+  updateAllCat,
+  deleteCat,
+} from "./cats.service";
 
 const router = Router();
 
 //Read 고양이 전체 데이터 조회
-router.get("/cats", (req, res) => {
-  try {
-    const cats = Cat;
-    // throw new Error("db connect error"); //error 발생
-    res.status(200).send({
-      success: true,
-      data: {
-        cats,
-      },
-    });
-  } catch (err: any) {
-    res.status(400).send({
-      success: false,
-      error: err.message,
-    });
-  }
-});
+router.get("/cats", readAllcat);
 
 //Read 특정 고양이 데이터 조회(동적 라우팅)
-router.get("/cats/:id", (req, res) => {
-  try {
-    const params = req.params;
-    const cat = Cat.find((cat) => {
-      return cat.id === params.id;
-    });
-    res.status(200).send({
-      success: true,
-      data: {
-        cat,
-      },
-    });
-  } catch (err: any) {
-    res.status(400).send({
-      success: false,
-      error: err.message,
-    });
-  }
-});
+router.get("/cats/:id", readCat);
 
 //CREATE 새로운 고양이 데이터 추가 API
-router.post("/cats", (req, res) => {
-  try {
-    const data = req.body;
-    Cat.push(data);
-    console.log(Cat);
-    res.status(200).send({
-      success: true,
-      data: { data },
-    });
-  } catch (err: any) {
-    res.status(400).send({
-      success: false,
-      error: err.message,
-    });
-  }
-});
+router.post("/cats", creatCat);
 
 //UPDATE 고양이 데이터 업데이트 -> PUT
-router.put("/cats/:id", (req, res) => {
-  try {
-    const params = req.params;
-    const body = req.body;
-    let result;
-
-    Cat.forEach((cat) => {
-      if (cat.id === params.id) {
-        cat = body;
-        result = cat;
-      }
-    });
-
-    res.status(200).send({
-      success: true,
-      data: {
-        success: true,
-        data: { result },
-      },
-    });
-  } catch (err: any) {
-    res.status(400).send({
-      success: false,
-      error: err.message,
-    });
-  }
-});
+router.put("/cats/:id", updateAllCat);
 
 //UPDATE 고양이 데이터 일부 업데이트 -> PATCH
-router.patch("/cats/:id", (req, res) => {
-  try {
-    const params = req.params;
-    const body = req.body;
-    let result;
-
-    Cat.forEach((cat) => {
-      if (cat.id === params.id) {
-        //중복된 key에 대한 value값을 바꾸도록 구조분해할당 사용
-        cat = { ...cat, ...body }; //구조분해할당 : 배열이나 객체의 속성을 해체하여 그 값을 개별 변수에 담을 수 있게하는 JavaScript 표현식
-        result = cat;
-      }
-    });
-
-    res.status(200).send({
-      success: true,
-      data: {
-        success: true,
-        data: { result },
-      },
-    });
-  } catch (err: any) {
-    res.status(400).send({
-      success: false,
-      error: err.message,
-    });
-  }
-});
+router.patch("/cats/:id", updateCat);
 
 //DELETE 고양이 데이터 삭제 -> DELETE
-router.delete("/cats/:id", (req, res) => {
-  try {
-    const params = req.params;
-    const newCat = Cat.filter((cat) => cat.id !== params.id);
-    res.status(200).send({
-      success: true,
-      data: { newCat },
-    });
-  } catch (err: any) {
-    res.status(400).send({
-      success: false,
-      error: err.message,
-    });
-  }
-});
+router.delete("/cats/:id", deleteCat);
 
 export default router; //라우터들을 export
