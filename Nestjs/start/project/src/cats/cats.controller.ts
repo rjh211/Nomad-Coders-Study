57857d1b7142f9710d7006f2213c426973aaf3e2
류종현 <rjh211@naver.com>
2022-08-10 22:45:12 +0900

    Add Exception and Parameter Validation

--- Nestjs/start/project/src/cats/cats.controller.ts
+++ Nestjs/start/project/src/cats/cats.controller.ts
@@ -1,16 +1,33 @@
+import { HttpExceptionFilter } from './../http-exception.filter';
 import { CatsService } from './cats.service';
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly CatsService: CatsService) {}
   @Get()
+  @UseFilters(HttpExceptionFilter) //하위 메서드에서 발생한 Exception이 내부에서 필터링됨(filter.ts의 @Catch(HTTPExceptionFilter)와 매칭), 해당 메서드에서만 적용 하는 방법
   getAllCat() {
+    throw new HttpException('api is broken', 401); //Nestjs에서의 Exception 처리방법
     return 'all cat';
   }
 
   @Get(':id') //동적라우팅
-  getCat() {
+  getCat(@Param('id', ParseIntPipe) param: number) {
+    //pipe를 통한 자동형변환 (id가 number type 일경우 자동변환)
+    //숫자가 아닌형태로 들어오는경우 유효성검사를 통해 오류를 발생시켜준다.(Exception Filter에서 오류 출력)
+    console.log(param);
     return 'one cat';
   }
 
--- Nestjs/start/project/src/main.ts
+++ Nestjs/start/project/src/main.ts
@@ -1,8 +1,10 @@
+import { HttpExceptionFilter } from './http-exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter()); //global로 exception filter 적용
   await app.listen(8000);
 }
 bootstrap();
