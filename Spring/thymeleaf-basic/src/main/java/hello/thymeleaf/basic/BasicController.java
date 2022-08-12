a115c1f6074305a9cbd3044f941b44d196e961f5
류종현 <rjh211@naver.com>
2022-08-12 11:10:21 +0900

    Add thymeleaf operation

--- Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -88,6 +88,14 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
 
         return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model){
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring");
+
+        return "basic/operation";
 
     }
 
