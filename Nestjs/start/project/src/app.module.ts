aabcff6252c2ac2b8605fa8be3678ed10205c576
류종현 <rjh211@naver.com>
2022-08-16 20:50:21 +0900

    use nestjs config

--- Nestjs/start/project/src/app.module.ts
+++ Nestjs/start/project/src/app.module.ts
@@ -9,16 +9,13 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     CatsModule,
-    // ConfigModule.forRoot(),
-    MongooseModule.forRoot(
-      'mongodb+srv://rjh211:whdgus12@nestcluster.xyfgmrg.mongodb.net/cats?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // useCreateIndex: true,
-        // useFindAndModify: false,
-      },
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true, mongoose 6.0이상 버전에서는 디폴트로 지원해주기 떄문에 값을 지워야 실행이된다.,
+      // useFindAndModify: false,
+    }),
   ], // cats.module에서  export하여 자동으로 app.module의 impmort에 추가가된다.(provider에 등록하지 않아도됨)
   controllers: [AppController],
   providers: [AppService],
