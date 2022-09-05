721b659e7d74c56c8646076936ce40a95129aeb8
류종현 <rjh211@naver.com>
2022-09-05 20:09:48 +0900

    Add message source and Add properties

--- Spring/message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ Spring/message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,9 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -10,4 +13,11 @@ public class ItemServiceApplication {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+//	@Bean
+//	public MessageSource messageSource(){ //Spring에서 디폴트로 지원하는 메세지 설정(자동으로 빈으로 등록해줌)
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasenames("message", "errors");
+//		messageSource.setDefaultEncoding("utf-8");
+//		return messageSource;
+//	}
 }
