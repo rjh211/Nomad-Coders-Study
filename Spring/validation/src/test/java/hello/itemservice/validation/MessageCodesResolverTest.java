8c149746b4bed081caecdbaaa47175901b3eaa19
류종현 <rjh211@naver.com>
2022-09-18 19:01:15 +0900

    Add MessageCodeResolver Test

---
+++ Spring/validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,28 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+public class MessageCodesResolverTest {
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("reuquired", "item");
+        //객체오류 - code.objectName - code 순으로 생성
+        Assertions.assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+        //필드오류 - code.object name.field - code.field - code.field.objectname - code 순으로 생성됨
+        Assertions.assertThat(messageCodes).containsExactly("required.item.itemName", "required.itemName","required.java.lang.String", "required");
+
+    }
+}
