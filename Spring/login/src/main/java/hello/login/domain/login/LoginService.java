c4cbcf221f69d09358549c8b31d92b16dc10a8cb
류종현 <rjh211@naver.com>
2022-10-14 23:37:44 +0900

    Add Login Function

---
+++ Spring/login/src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,25 @@
+package hello.login.domain.login;
+
+import hello.login.web.member.Member;
+import hello.login.web.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password){
+        //null일 경우 로그인실패
+//        Optional<Member> findMemberOptional = memberRepository.findByLoginId(loginId);
+//        Member member = findMemberOptional.get();
+//        if(member.getPassword().equals(password)){
+//            return member;
+//        }
+//        return null;
+        return memberRepository.findByLoginId(loginId).filter(m->m.getPassword().equals(password)).orElse(null);
+    }
+}
