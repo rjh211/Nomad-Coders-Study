3cc23a7eff7dd6184c380a72a88d4fb51bc77285
류종현 <rjh211@naver.com>
2022-10-21 00:09:05 +0900

    ADD Session Attribute

--- Spring/login/src/main/java/hello/login/web/HomeController.java
+++ Spring/login/src/main/java/hello/login/web/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -51,7 +52,7 @@ public class HomeController {
         model.addAttribute("member", member);
         return "loginHome";
     }
-    @GetMapping("/")
+    //    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model){
 
         HttpSession session = request.getSession(false);//로그인하지 않은 사용자까지 세션을 생성할 필요가 없기 때문에 false
@@ -63,6 +64,16 @@ public class HomeController {
         Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
 
         //Session Manager에 저장된 회원정보 조회
+
+        if(loginMember == null){
+            return "home";
+        }
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+        @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model){
+        //@SessionAttribute Annotation이 세션생성및 제공을 다해줌
 
         if(loginMember == null){
             return "home";
