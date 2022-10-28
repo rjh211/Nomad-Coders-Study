91af1bab0b56bec1b96d39cd7073f35c19689ad1
류종현 <rjh211@naver.com>
2022-10-28 23:25:47 +0900

    ADD Spring Interceptor

--- Spring/login/src/main/java/hello/login/WebConfig.java
+++ Spring/login/src/main/java/hello/login/WebConfig.java
@@ -2,19 +2,27 @@ package hello.login;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.iterceptor.LoginInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterRegistration;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 //Filter 등록 Config 파일
 
-//@WebFilter Annotation으로 필터등록을 해도 되지만, 순서를 지정할수 없기 때문에 WebConfig를 사용하는 방식이 더 낫다.
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()).order(1).addPathPatterns("/**").excludePathPatterns("/css/**", "/*.ico", "/error");//Path Pattern이 서블릿과 다름, **을 붙여야함
+    }
+
+    //@WebFilter Annotation으로 필터등록을 해도 되지만, 순서를 지정할수 없기 때문에 WebConfig를 사용하는 방식이 더 낫다.
+//    @Bean log를 Interceptor로 쓰기위해 빈 제거
     public FilterRegistrationBean logFilter(){
         //Spring BOOT가 WAS를 띄울때 필터를 넣어줌
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
