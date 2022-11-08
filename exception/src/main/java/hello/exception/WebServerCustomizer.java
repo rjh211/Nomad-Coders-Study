dea2bae526ca70027dfee102c6dd8927db23d1a0
류종현 <rjh211@naver.com>
2022-11-08 22:31:57 +0900

    Add Spring Error Page

--- exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component SpringBoot의 BasicErrorController 사용을 위해 주석처리
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
