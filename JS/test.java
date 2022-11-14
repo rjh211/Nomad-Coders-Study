1e6872f454f0a82acf8201aca781c0c039cb4a0c
류종현 <rjh211@naver.com>
2022-11-14 21:47:26 +0900

    Add Default Exception Handler Resolver

---
+++ JS/test.java
@@ -0,0 +1,10 @@
+class test{
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+    public int solution(String[] babbling){
+        int answer = 0;
+        return answer;
+    }
+}
