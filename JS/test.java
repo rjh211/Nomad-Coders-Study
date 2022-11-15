1bc9f7649e3a1ffedbfffb6218c4c4b76f20f6f2
류종현 <rjh211@naver.com>
2022-11-15 20:35:04 +0900

    Add Exception Handler

--- JS/test.java
+++ JS/test.java
@@ -1,10 +1,27 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 class test{
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println(Solution.solution(new int[]{0,-1,-2,-3}));
     }
 
-    public int solution(String[] babbling){
-        int answer = 0;
-        return answer;
+}
+class Solution{
+    static int solution(int[] common){
+        Set<Integer> plusList = new HashSet<Integer>();
+        Set<Integer> productList = new HashSet<Integer>();
+        for(int i = 0 ; i < common.length - 1; i++){
+            plusList.add(common[i + 1] - common[i]);
+            if(common[i] != 0){
+                productList.add(common[i + 1] / common[i]);
+            }
+        }
+
+        Iterator<Integer> iter = plusList.size() == 1 ? plusList.iterator() : productList.iterator();
+
+        return plusList.size() == 1 ? common[common.length - 1] + iter.next() : common[common.length - 1] * iter.next();
     }
 }
+
