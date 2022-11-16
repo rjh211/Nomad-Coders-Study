2f2031e48b2945bc77414bd63f15952accb363cb
류종현 <rjh211@naver.com>
2022-11-16 10:38:34 +0900

    Add RestControllerAdvice

--- exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -11,30 +11,31 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 public class ApiExceptionV2Controller {
-
-    //해당 메서드가 호출될경우 HttpStatus 지정
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegeExHandler(IllegalArgumentException e){
-        //컨트롤러에서 ExceptionHandler 파라미터에 대한 에러 발생 시 본 메서드가 실행된다.
-        //ExceptionHandlerExceptionResolver에서 자신이 해결할 수 잇는 Exception이 해당 컨트롤러에 존재하는지 찾은 후 있다면 실행
-        log.error("[exceptionHandler] ex", e);
-        return new ErrorResult("BAD", e.getMessage());
-    }
-
-    @ExceptionHandler/*(UserException.class)생략가능 메서드의 파리미터로 Exception을 넣을 경우*/
-    public ResponseEntity<ErrorResult> userExHandler(UserException e){
-        log.error("[exceptionHandler] ex", e);
-        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
-    public ErrorResult exHandler(Exception e){
-        log.error("[exceptionHandler] ex", e);
-        return new ErrorResult("EX", "@ExceptionHandler로 잡지 않은 나머지 예외 일괄처리(최상위 Exception 이기 떄문)");
-    }
+//     예외처리 로직을 ExControllerAdvice에 모아서 처리하도록 설정함
+//
+//    //해당 메서드가 호출될경우 HttpStatus 지정
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    @ExceptionHandler(IllegalArgumentException.class)
+//    public ErrorResult illegeExHandler(IllegalArgumentException e){
+//        //컨트롤러에서 ExceptionHandler 파라미터에 대한 에러 발생 시 본 메서드가 실행된다.
+//        //ExceptionHandlerExceptionResolver에서 자신이 해결할 수 잇는 Exception이 해당 컨트롤러에 존재하는지 찾은 후 있다면 실행
+//        log.error("[exceptionHandler] ex", e);
+//        return new ErrorResult("BAD", e.getMessage());
+//    }
+//
+//    @ExceptionHandler/*(UserException.class)생략가능 메서드의 파리미터로 Exception을 넣을 경우*/
+//    public ResponseEntity<ErrorResult> userExHandler(UserException e){
+//        log.error("[exceptionHandler] ex", e);
+//        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+//        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
+//    }
+//
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    @ExceptionHandler
+//    public ErrorResult exHandler(Exception e){
+//        log.error("[exceptionHandler] ex", e);
+//        return new ErrorResult("EX", "@ExceptionHandler로 잡지 않은 나머지 예외 일괄처리(최상위 Exception 이기 떄문)");
+//    }
 
     @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id){
