f93e1ef25497e7017ee817998e27553ed29bb974
류종현 <rjh211@naver.com>
2022-11-18 00:21:19 +0900

    add legacy Transformation Data Type

--- JS/test.java
+++ JS/test.java
@@ -1,21 +1,33 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class test{
     public static void main(String[] args) {
-        System.out.println(Solution.solution(new int[][]{{0, 1}, {3, 9}, {2, 5}}));
+        for(String s : Solution.solution(new String[]{"3 - 4 = -3","5 + 6 = 11"})){
+            System.out.println(s + " ");
+        }
     }
     
     class Solution {
-        static int result = 0;
-        public static int solution(int[][] lines) {
-            int[] line = new int[201];
-
-            for(int i = 0 ; i < lines.length; i++){
-                for(int j = lines[i][0]; j <= lines[i][1] ; j++){
-                    line[j + 100]++;
+        public static String[] solution(String[] quiz) {
+            String[] answer = new String[quiz.length];
+            for(int i = 0; i< quiz.length; i++){
+                String[] parseStr = quiz[i].split(" = ");
+                int result = Integer.parseInt(parseStr[1]);
+                String[] strArr = null;
+                int value = 0;
+                if(parseStr[0].contains("+")){
+                    parseStr[0] = parseStr[0].replace(" + ", " - ");
+                    strArr = parseStr[0].split(" - ");
+                    value += Integer.parseInt(strArr[0]) + Integer.parseInt(strArr[1]);
+                } else {
+                    strArr = parseStr[0].split(" - ");
+                    value += Integer.parseInt(strArr[0]) - Integer.parseInt(strArr[1]);
                 }
+
+                answer[i] = value == result ? "O" : "X";
             }
-            return (int) Arrays.stream(line).filter(e-> e > 1).count();
+            return answer;
         }
     }
 }
