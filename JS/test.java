31fcbdbebe7897754d6be973b1ac34cba1bb2a68
류종현 <rjh211@naver.com>
2022-11-17 20:22:00 +0900

    Add TypeConverter Project

--- JS/test.java
+++ JS/test.java
@@ -1,28 +1,21 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 class test{
     public static void main(String[] args) {
-        for (int num : test.solution(0, 3, 1, 4)) {
-            System.out.println(num);
-        }
+        System.out.println(Solution.solution(new int[][]{{0, 1}, {3, 9}, {2, 5}}));
     }
-    public static int[] solution(int denum1, int num1, int denum2, int num2) {
-        int up = denum1 * num2 + denum2 * num1;
-        int down = num1 * num2;
-        int num = 2;
-        int small_num = num1 > num2 ? num2 : num1;
-        while(num <= small_num){
-            if(up == 1 || down == 1) break;
-            if(up % num + down % num == 0){
-                up /= num;
-                down /= num;
-                continue;
-            }
-            num++;
-        }
-        return new int[] {up, down};
-    }
+    
+    class Solution {
+        static int result = 0;
+        public static int solution(int[][] lines) {
+            int[] line = new int[201];
 
+            for(int i = 0 ; i < lines.length; i++){
+                for(int j = lines[i][0]; j <= lines[i][1] ; j++){
+                    line[j + 100]++;
+                }
+            }
+            return (int) Arrays.stream(line).filter(e-> e > 1).count();
+        }
+    }
 }
