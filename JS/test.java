103a3dfefef8cc4671c8dae94eb6f92eac4b727f
류종현 <rjh211@naver.com>
2022-11-24 23:13:37 +0900

    Add Formatter

--- JS/test.java
+++ JS/test.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 class test{
     public static void main(String[] args) {
-        // System.out.println(Solution.solution(new int[]{1,2,3,4,5,6}, 4));
-        for(int s : Solution.solution(new int[]{1,2,3,4,5,6}, 4)){
+        // System.out.println(Solution.solution(8, new int[]{1, 3, 7}));
+        for(int s : Solution.solution(2, new int[]{1})){
             System.out.println(s + " ");
         }
         // for(int i : IntStream.rangeClosed(1, 10).toArray()){
@@ -14,23 +18,33 @@ class test{
     }
     
     class Solution {
-        public static int[] solution(int[] numlist, int n) {
-            Integer[] resultArray = Arrays.stream(numlist).boxed().toArray(Integer[]::new);
-            Arrays.sort(resultArray, new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    // TODO Auto-generated method stub
-                    if(Math.abs(o1 - n) > Math.abs(o2 - n)){
-                        return 1;
-                    } else if (Math.abs(o1 - n) == Math.abs(o2 - n)){
-                        return o1 < o2 ? 1 : -1;
-                    } else {
-                        return -1;
+        public static int[] solution(int e, int[] starts) {
+            int[] answer = new int[starts.length];
+            int[] basicArray = new int[e - starts[0] + 1];
+            Arrays.sort(starts);
+            for(int j = 0; j < starts.length; j++){
+                int[] maxValue = new int[]{-1, 0};
+                for(int i = starts[j] - starts[0]; i < basicArray.length; i++){
+                    int methodValue = method(i + starts[0]);
+                    basicArray[i] = methodValue;
+                    if(methodValue > maxValue[0]){
+                        maxValue[0] = methodValue;
+                        maxValue[1] = i + starts[0];
                     }
                 }
-                
-            });
-            return Arrays.stream(resultArray).mapToInt(Integer::intValue).toArray();
+                answer[j] = maxValue[1];
+            }
+            
+            return answer;
+        }
+
+        public static int method(int number){
+            int count = 0;
+            for (int i = 1; i * i <= number; i++) {
+                if (i * i == number) count++;
+                else if (number % i == 0) count += 2;
+            }
+            return count;
         }
     }
 }
