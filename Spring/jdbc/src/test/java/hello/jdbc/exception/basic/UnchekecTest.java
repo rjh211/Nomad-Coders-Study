57d3222335c489a9f6945f8e5a2ae66c364eecc8
류종현 <rjh211@naver.com>
2023-01-05 17:50:17 +0900

    Add Unchecked Exception

---
+++ Spring/jdbc/src/test/java/hello/jdbc/exception/basic/UnchekecTest.java
@@ -0,0 +1,53 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class UnchekecTest {
+    static class MyUncheckedException extends RuntimeException {
+        public MyUncheckedException(String message) {
+            super(message);
+        }
+    }
+    /*
+     * Unchecked 예외는 예외를 잡거나 던지지 않아도 된다.
+     * 자동으로 밖으로 던져진다.
+     */
+    static class Service{
+        /**
+         * 필요한 경우 예외를 잡아서 처리하면된다.
+         */
+        Repository repository = new Repository();
+        public void callCatch(){
+            try{
+                repository.call();
+            } catch(MyUncheckedException e){
+                log.info("예외 처리 , message={}", e.getMessage(), e);
+            }
+        }
+
+        public void callThrow(){
+            repository.call();
+        }
+    }
+
+    static class Repository{
+        public void call(){
+            throw new MyUncheckedException("ex");
+        }
+    }
+
+    @Test
+    void unchecked_catch(){
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void unchecked_throw(){
+        Service service = new Service();
+        Assertions.assertThatThrownBy(()->service.callThrow()).isInstanceOf(MyUncheckedException.class);
+    }
+}
