cb6ca2a8ea5e0607cbfe6503e302e1dce51c07f3
류종현 <rjh211@naver.com>
2023-06-01 23:20:08 +0900

    Send Single Message

--- kafka/KafkaProj-01/producers/src/main/java/com/example/kafka/SimpleProducer.java
+++ kafka/KafkaProj-01/producers/src/main/java/com/example/kafka/SimpleProducer.java
@@ -2,12 +2,15 @@ package com.example.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
 public class SimpleProducer {
     public static void main(String[] args) {
+        String topicName = "simple-topic";
+
         //kafka producer object configuration setting
         Properties props = new Properties();
 
@@ -16,5 +19,19 @@ public class SimpleProducer {
         props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
         props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        //숫자 사용시 IntegerSerializer등 사용
+
+        //KafkaProducer 객체 생성
+        //key = null, value = "hello world"
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(props);
+
+        //producerRecord 객체 생성. (Producer의 key, value 타입이 같아야한다.)
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, "id-001", "hello world 2");
+
+        //kafkaproducer message send
+        kafkaProducer.send(producerRecord);
+
+        kafkaProducer.flush();
+        kafkaProducer.close();
     }
 }
